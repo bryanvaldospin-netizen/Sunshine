d4e98abf5cb699e0350cf40e264579590dfc568f
Yareel Valdospin <bryan_valdospin@hotmail.com>
2026-03-06 03:53:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2504766329-6c1a7",
+  "appId": "1:728874645997:web:fe54f77ab8e448e4d5cfe4",
+  "apiKey": "AIzaSyBPCw5W2WS5Za4MkQIT8zLzcNw78dtwf6o",
+  "authDomain": "studio-2504766329-6c1a7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "728874645997"
+};
