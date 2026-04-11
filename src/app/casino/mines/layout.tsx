ba617844fcd126103a6c86e5f90a614b7cf4417e
Yareel Valdospin <bryan_valdospin@hotmail.com>
2026-04-11 02:57:27 +0000

    elimnalo de la pagina de casino, casino/roulette y casino/mines

--- src/app/casino/mines/layout.tsx
+++ src/app/casino/mines/layout.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { Header } from '@/components/header';
 import { useAuth } from '@/hooks/use-auth';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
@@ -21,7 +20,6 @@ export default function MinesLayout({ children }: { children: React.ReactNode })
 
   return (
     <div className="relative flex min-h-screen flex-col bg-gray-900 text-white">
-      <Header />
       <div className="flex-1">{children}</div>
     </div>
   );
